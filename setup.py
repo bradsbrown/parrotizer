01919d893286a5c726afac25d95f2f2cc6470349
Brad Brown <brad.brown@rackspace.com>
2018-08-28 21:02:34 -0500

    Enhancement: readme and license

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ EXTRAS_REQUIRE = {}
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 about = {}
 if not VERSION:
     with open(os.path.join(here, NAME, '__version__.py')) as f:
@@ -25,20 +28,27 @@ else:
     about['__version__'] = VERSION
 
 
-setuptools.setup(name=NAME,
-                 version=about['__version__'],
-                 description=DESCRIPTION,
-                 url='https://github.com/bradsbrown/parrotizer',
-                 author='Brad Brown',
-                 author_email='brad@bradsbrown.com',
-                 license='MIT',
-                 entry_points={
-                     'console_scripts': CONSOLE_SCRIPTS,
-                 },
-                 install_requires=INSTALL_REQUIRES,
-                 packages=setuptools.find_packages(),
-                 include_package_data=True,
-                 zip_safe=False,
-                 extras_require=EXTRAS_REQUIRE,
-                 )
-
+setuptools.setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bradsbrown/parrotizer',
+    author='Brad Brown',
+    author_email='brad@bradsbrown.com',
+    license='MIT',
+    entry_points={
+        'console_scripts': CONSOLE_SCRIPTS,
+    },
+    install_requires=INSTALL_REQUIRES,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=EXTRAS_REQUIRE,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
